17801c56fb179beff25b78d11aa2f93503ccc42f
vitaly <vitaly.samsonenko@gmail.com>
2021-04-21 02:43:31 +0200

    Add project files.

---
+++ BettingGame/Guy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoGuysGiveCashToEachOther
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+        /// <summary>
+        /// Writes my name and the amount of cash I have to the console
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine("{0} has {1} bucks", Name, Cash);
+        }
+        /// <summary>
+        /// Gives some of my cash, removing it from my wallet, if I have enough
+        /// </summary>
+        /// <param name="amount">Amount of cash to give</param>
+        /// <returns>Amount of cash removed from my wallet</returns>
+        public int GiveCash(int amount) 
+        {
+            if (amount < 0) 
+            {
+                Console.WriteLine("{0} says: the amount {1} is too negative", Name, amount);
+                return 0;
+            }
+            if (amount > Cash) 
+            {
+                Console.WriteLine("{0} says: I don't have enough cash to give you {1} bucks", Name, amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+
+        }
+        /// <summary>
+        /// Receive some cash, adding it to my wallet
+        /// </summary>
+        /// <param name="amount"></param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("{0} says: {1} is not the amount that I'm going to take", Name, amount);
+                return;
+            }
+            Cash += amount;
+
+        }
+    }
+}
---
+++ BettingGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TwoGuysGiveCashToEachOther;
+
+namespace BettingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Guy player = new Guy() {Name = name, Cash = 100 };
+            Console.WriteLine("Hello {0} and welcome to our casino. The odds are {1}", player.Name, odds);
+
+            while (player.Cash > 0) 
+            {
+                player.WriteMyInfo();
+                Console.WriteLine("How much do you want to bet:");
+                string howMuch = Console.ReadLine();
+                if (!int.TryParse(howMuch, out int amount)) 
+                {
+                    Console.WriteLine("{0} is not a correct amount. Try again");
+                    continue;
+                }
+                int pot = player.GiveCash(amount)*2;
+
+
+                if (random.NextDouble() > odds) 
+                {
+                    Console.WriteLine("You win");
+                    player.ReceiveCash(pot);
+                }
+                else 
+                {
+                    Console.WriteLine("Bad luck. You lose");
+                    
+                }
+
+ 
+            }
+            Console.WriteLine("{0} ran out of cash.\n House always wins", player.Name);
+            
+            
+        }
+    }
+}
